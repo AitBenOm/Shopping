04a223c77cdae4b89f42f58b6e57a7e82f5466a3
AitBenOm <omar.benaissa@outlook.com>
2017-10-20 16:12:57 +0200

    Commit- 21/09/2017

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +10,22 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // const id = this.route.snapshot.params['id'];
+
+  this.route.params.subscribe(
+      (params: Params) => {
+        console.log('detail component ' + this.recipeService.getRecipeById(+params['id']));
+        this.recipe = this.recipeService.getRecipeById(+params['id']);
+      }
+    );
   }
+
+
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
